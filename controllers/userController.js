a18c1e521feb320cae81d649c09bd36e4ab124ce
Aditya Mahamuni <aditya.mahamuni1@mitwpu.edu.in>
2026-06-26 20:13:08 +0530

    changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -36,7 +36,7 @@ const resetPasswordSchema = z.object({
 const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "Strict",
+  sameSite: "None",
   maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
 };
 
